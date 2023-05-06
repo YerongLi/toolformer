7e8369542f21424917dcf3a21aa2bd44b70adbd6
Yerong <yerong.li@outlook.com>
2023-05-05 22:03:50 -0500

    remove duplicate casting prompt_ids to self.device

--- example.py
+++ example.py
@@ -42,6 +42,7 @@ calculator_api = CalculatorAPI(
 
 model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m")
 tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
+logging.info(model.device)
 
 # model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
 # tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
